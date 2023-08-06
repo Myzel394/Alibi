1769b2167cf830f9928a6c9a2b92535712153ac9
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-08-06 23:49:17 +0200

    feat: Add information label

--- app/src/main/java/app/myzel394/alibi/ui/components/AudioRecorder/molecules/StartRecording.kt
+++ app/src/main/java/app/myzel394/alibi/ui/components/AudioRecorder/molecules/StartRecording.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Mic
@@ -21,6 +22,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -28,8 +30,11 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import app.myzel394.alibi.R
+import app.myzel394.alibi.dataStore
+import app.myzel394.alibi.db.AppSettings
 import app.myzel394.alibi.services.RecorderService
 import app.myzel394.alibi.ui.BIG_PRIMARY_BUTTON_SIZE
 import app.myzel394.alibi.ui.components.AudioRecorder.atoms.AudioVisualizer
@@ -44,10 +49,7 @@ fun StartRecording(
     service: RecorderService? = null,
 ) {
     val context = LocalContext.current
-
     val saveFile = rememberFileSaverDialog("audio/*")
-
-    val hasAmplitudes = service?.amplitudes?.isNotEmpty() ?: false
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -97,6 +99,22 @@ fun StartRecording(
                 }
             }
         }
+        val settings = LocalContext
+            .current
+            .dataStore
+            .data
+            .collectAsState(initial = AppSettings.getDefaultInstance())
+            .value
+        Text(
+            stringResource(R.string.ui_audioRecorder_action_start_description, settings.audioRecorderSettings.maxDuration / 1000 / 60),
+            style = MaterialTheme.typography.bodySmall.copy(
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+            ),
+            modifier = Modifier
+                .widthIn(max = 300.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
+        )
         if (service?.recordingStart != null)
             Column(
                 modifier = Modifier.weight(1f),
